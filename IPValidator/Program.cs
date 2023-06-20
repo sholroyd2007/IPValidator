20a56959409289b2fbe38429c867215761bcfcd8
Shaun Holroyd <shaun@redskykh.com>
2023-06-20 15:38:01 +0700

    Add project files.

---
+++ IPValidator/Program.cs
@@ -0,0 +1,31 @@
+﻿bool result = true;
+
+Console.WriteLine("Enter IP Address for Validation:");
+var input = Console.ReadLine();
+
+if (input == null)
+{
+    result = false;
+}
+
+var strings = input.Split('.');
+
+if (strings.Length != 4)
+{
+    result = false;
+}
+
+foreach (var s in strings)
+{
+    if (s.Substring(0, 1) == "0" || Int32.Parse(s) > 255 || Int32.Parse(s) < 0)
+    {
+        result = false;
+    }  
+}
+
+Console.WriteLine("Is IP Address Valid?");
+Console.WriteLine(result);
+
+
+
+
